905a2ccc37e067f5e0936468cc2a270e9f25a31d
Ahn HyungJun <ahj3579@naver.com>
2024-10-28 16:49:23 +0900

    [#176]feat: emailController 구현

--- src/main/java/com/lovecloud/email/application/EmailSetService.java
+++ src/main/java/com/lovecloud/email/application/EmailSetService.java
@@ -1,7 +1,9 @@
 package com.lovecloud.email.application;
 
+import com.lovecloud.auth.application.UserPasswordChangeService;
 import com.lovecloud.email.application.command.PasswordResetCommand;
 import com.lovecloud.email.domain.EmailMessage;
+import com.lovecloud.global.util.UUIDUtil;
 import com.lovecloud.usermanagement.application.UserQueryService;
 import com.lovecloud.usermanagement.domain.User;
 import lombok.RequiredArgsConstructor;
@@ -12,20 +14,21 @@ import org.springframework.stereotype.Service;
 public class EmailSetService {
 
     private final UserQueryService userQueryService;
+    private final UserPasswordChangeService userPasswordChangeService;
 
     public EmailMessage setEmail(final PasswordResetCommand passwordResetCommand){
 
         User user = userQueryService.getUserInfoByEmail(passwordResetCommand.email());
 
-        String tempPassword = "1234"; // 임시 비밀번호 "1234"
+        String tempPassword = UUIDUtil.generateShortUUID();
         String email = passwordResetCommand.email();
         String name = passwordResetCommand.name();
 
-        //TODO: 임시 비밀번호로 변경 로직 추가
+        userPasswordChangeService.changePassword(user, tempPassword);
 
         return EmailMessage.builder()
                 .toEmail(email)
-                .subject("[LoveCloud] 임시 비밀번호")
+                .subject("[LoveCloud] 임시 비밀번호 발송")
                 .message("안녕하세요. " + name + "님! \n" +
                         "임시 비밀번호는 " + tempPassword + " 입니다. \n" +
                         "로그인 후 비밀번호를 변경해주세요.")
--- src/main/java/com/lovecloud/email/presentation/EmailController.java
+++ src/main/java/com/lovecloud/email/presentation/EmailController.java
@@ -1,4 +1,30 @@
 package com.lovecloud.email.presentation;
 
+import com.lovecloud.email.application.EmailSendService;
+import com.lovecloud.email.application.EmailSetService;
+import com.lovecloud.email.domain.EmailMessage;
+import com.lovecloud.email.presentation.request.PasswordResetRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/email")
 public class EmailController {
+
+    private final EmailSendService emailSendService;
+    private final EmailSetService emailSetService;
+
+    @PostMapping("/password-reset")
+    public ResponseEntity<Long> sendTempPasswordEmail(@Valid @RequestBody PasswordResetRequest request) {
+
+        EmailMessage emailMessage = emailSetService.setEmail(request.toCommand());
+        emailSendService.sendEmail(emailMessage);
+        return ResponseEntity.ok().build();
+    }
 }
